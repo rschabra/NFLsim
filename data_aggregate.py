6080c84fadded3ab556d56990851142119223103
Rohan Chabra <rschabra@umd.edu>
2021-05-21 17:38:45 -0400

    Initial stats commit

--- data_aggregate.py
+++ data_aggregate.py
@@ -3,8 +3,8 @@ from csv import writer
 from array import *
 
 teams2020 = Teams(year = '2020')
-
-props = ['team.name', 'player.name', 'assists_on_tackles', 'attempted_passes', 'completed_passes', 'extra_points_attempted',
+elems = [0]*48
+props = ['name', 'assists_on_tackles', 'attempted_passes', 'completed_passes', 'extra_points_attempted',
         'extra_points_made', 'field_goals_attempted', 'field_goals_made', 'fumbles', 'fumbles_forced', 
         'fumbles_recovered', 'fumbles_recovered_for_touchdown', 'interceptions', 'interceptions_returned_for_touchdown', 'interceptions_thrown', 
         'kickoff_return_touchdown', 'kickoff_return_yards', 'kickoff_returns', 'longest_interception_return', 'longest_kickoff_return', 
@@ -13,14 +13,25 @@ props = ['team.name', 'player.name', 'assists_on_tackles', 'attempted_passes', '
         'receptions', 'rush_attempts', 'rush_touchdowns', 'rush_yards', 'sacks', 'times_pass_target', 'times_sacked', 'total_punt_yards', 'yards_per_punt', 
         'yards_per_punt_return', 'yards_recovered_from_fumble', 'yards_returned_from_interception']
 
-with open('player_data.csv', 'w', newline='') as write_arr:
+with open('/Users/rohanchabra/nflwinnersim/player_data.csv', 'w', newline='') as write_arr:
         csv_writer = writer(write_arr)
-        csv_writer.writerow(props)
+        csv_writer.writerow(['team.name'] + props)
 
 def append_arr_as_row(file_name, arr_elem, newline=''):
     with open(file_name, 'a+', newline='') as write_arr:
         csv_writer = writer(write_arr)
         csv_writer.writerow(arr_elem)
 
-
+for team in teams2020:
+    elems[0] = team.name
+    for player in team.roster.players:
+        g = 1
+        for prop in props:
+            try:
+                elems[g] = getattr(player, prop)
+            except:
+                elems[g] = 0
+            g += 1
+        append_arr_as_row('/Users/rohanchabra/nflwinnersim/player_data.csv', elems)
+   
 
